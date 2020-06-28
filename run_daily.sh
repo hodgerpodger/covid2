f074eb22a880b1a62faebccb1aa86ffbaedfd809
Roger Hong <rogerhong@gmail.com>
2020-06-27 22:08:25 -0700

    Move log path to script instead of cronjob

--- run_daily.sh
+++ run_daily.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
 
-
+# Wrapper script to excecute run.py from a cronjob
+set -euo pipefail
 
 cd ~/github/covid2
 source env/bin/activate
-
 git pull
 
-python update.py
+python update.py >> /tmp/covid2.stdout.log 2>> /tmp/covid2.stderr.log
+
 cd docs
 php index.php > index.html
 
